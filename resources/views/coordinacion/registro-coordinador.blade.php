33b926a80865775718628d483d6474350bd4c74e
Angel05m <aasm052002@gmail.com>
2025-11-20 00:12:30 -0700

    feat: cambios en los formularios

--- resources/views/coordinacion/registro-coordinador.blade.php
+++ resources/views/coordinacion/registro-coordinador.blade.php
@@ -4,56 +4,60 @@
 
 @section('content')
 
-<section class="flex-1 flex justify-center items-center mt-10">
-    <x-msj-alert />
-    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-3xl">
-        <div class="flex justify-between items-center mb-6">
-            <div>
-                <h1 class="text-3xl font-bold text-black">Registro de Coordinador</h1>
-                <span class="text-gray-500 font-light pt-2 block">Complete el siguiente formulario</span>
-            </div>
-            <a href="{{route('coordinacion.lista-coordinador')}}" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 shadow">
-                Cancelar
-            </a>
-        </div>
-        <form id="form-insertar" data-url="{{ route('coordinacion.guardar-coordinador') }}" class="w-full gap-6" action="" method="post">
-            @csrf
-            <div class="flex flex-col gap-2">
-                <label class="text-gray-500 font-light">Nombre: *</label>
-                <input
-                    class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    type="text" name="nombres" required
-                >
-
-                <label class="text-gray-500 font-ligh">Apellido paterno: *</label>
-                <input
-                    class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    type="text" name="ap_paterno" required
-                >
-
-                <label class="text-gray-500 font-light">Apellido materno:</label>
-                <input
-                    class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    type="text" name="ap_materno"
-                >
-                <label class="text-gray-500 font-light">Correo electrónico: *</label>
-                <input
-                    class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    type="email" name="email" required
-                >
-                <div class="flex justify-center mt-6">
-                    <button
-                        class="bg-teal-600 text-white w-full px-6 py-2 rounded-lg hover:bg-teal-700 shadow"
-                        type="submit"
-                    >
-                        Registrar
-                    </button>
+    <section class="flex-1 flex justify-center items-center mt-10">
+        <x-msj-alert />
+        <div class="bg-white p-6 sm:p-8 rounded-lg shadow-md w-full max-w-3xl">
+            <div class="flex items-center w-full mb-6">
+                <div class="flex flex-row items-center w-full gap-2">
+                    <a href="{{ route('coordinacion.lista-coordinador') }}" class="w-auto flex justify-center items-center">
+                        <svg class="w-8 h-8 text-teal-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m15 19-7-7 7-7" />
+                        </svg>
+                        <span class="text-teal-600 font-light hidden sm:block">Regresar</span>
+                    </a>
+                    <div class="flex justify-center items-center w-full">
+                        <h1 class="text-2xl sm:text-4xl font-bold text-teal-600">Registro de Coordinador</h1>
+                    </div>
                 </div>
+                {{-- <a href="{{route('coordinacion.lista-coordinador')}}" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 shadow">
+                Cancelar
+            </a> --}}
             </div>
+            <form id="form-insertar" data-url="{{ route('coordinacion.guardar-coordinador') }}" class="w-full gap-6"
+                action="" method="post">
+                @csrf
+                <div class="flex flex-col gap-2">
+                    <label class="text-gray-500 font-light">Nombre: *</label>
+                    <input
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        type="text" name="nombres" required>
+
+                    <label class="text-gray-500 font-ligh">Apellido paterno: *</label>
+                    <input
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        type="text" name="ap_paterno" required>
+
+                    <label class="text-gray-500 font-light">Apellido materno:</label>
+                    <input
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        type="text" name="ap_materno">
+                    <label class="text-gray-500 font-light">Correo electrónico: *</label>
+                    <input
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        type="email" name="email" required>
+                    <div class="flex justify-center mt-6">
+                        <button class="bg-teal-600 text-white w-full px-6 py-2 rounded-lg hover:bg-teal-700 shadow cursor-pointer"
+                            type="submit">
+                            Registrar
+                        </button>
+                    </div>
+                </div>
 
 
-        </form>
-    </div>
-</section>
+            </form>
+        </div>
+    </section>
 
 @endsection
--- resources/views/coordinacion/registro-grupo.blade.php
+++ resources/views/coordinacion/registro-grupo.blade.php
@@ -4,95 +4,104 @@
 
 @section('content')
 
-<section class="flex-1 flex justify-center items-center mt-12">
-    <x-msj-alert />
-    <div class="bg-white p-8 rounded-lg shadow-md w-full max-w-3xl">
-        <div class="flex justify-between items-center mb-6">
-            <div>
-                <h1 class="text-3xl font-bold text-black">Crea un Grupo</h1>
-                <span class="text-gray-500 font-light pt-2 block">Complete el siguiente formulario</span>
-            </div>
-            <a href="{{route('coordinacion.lista-grupos')}}" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 shadow">
-                Cancelar
-            </a>
-        </div>
-        <form id="form-insertar" data-url="{{ route('coordinacion.guardar-grupo') }}" class="w-full gap-6" action="" method="post">
-            @csrf
-            <div class="flex flex-col gap-2">
-                <label class="text-gray-500 font-light">Nombre del grupo:</label>
-                <select
-                class="p-2 pr-10 border border-gray-300 rounded-lg w-full sm:w-full focus:outline-none focus:ring-2 focus:ring-teal-500 transition text-base appearance-none"
-                    name="nombre" id="">
-                    <option value="">Seleccione un nombre</option>
-                    <option value="A">A</option>
-                    <option value="B">B</option>
-                    <option value="C">C</option>
-                    <option value="D">D</option>
-                    <option value="E">E</option>
-                    <option value="F">F</option>
+    <section class="flex-1 flex justify-center items-center mt-8 sm:mt-12">
+        <x-msj-alert />
+        <div class="bg-white p-3 sm:p-8 rounded-lg shadow-md w-full max-w-3xl">
+            <div class="flex w-full items-center mb-6">
+                <div class="flex flex-row items-center w-full gap-2">
+                    <a href="{{route('coordinacion.lista-grupos')}}" class="w-auto flex justify-center items-center">
+                        <svg class="w-8 h-8 text-teal-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                            height="24" fill="none" viewBox="0 0 24 24">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="m15 19-7-7 7-7" />
+                        </svg>
+                        <span class="text-teal-600 font-light hidden sm:block">Regresar</span>
+                    </a>
+                    <div class="flex justify-start sm:justify-center items-center w-full">
+                        <h1 class="text-3xl sm:text-5xl font-bold text-teal-600">Crea un Grupo</h1>
+                    </div>
 
-                </select>
-
-                <label class="text-gray-500 font-light" for="">Carrera:</label>
-                <select
-                class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    name="fk_carrera" id="">
-                    <option value="">Seleccione una carrera</option>
-                    @foreach ($carreras as $carrera)
-                        <option value="{{ $carrera->pk_carrera }}">
-                            {{ $carrera->nombre}} - {{ $carrera->abreviatura}}
-                        </option>
-                    @endforeach
-                </select>
-                <label class="text-gray-500 font-light">Cuatrimestre:</label>
-                <select
-                class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    name="fk_cuatrimestre" id="">
-                    <option value="">Seleccione un cuatrimestre</option>
-                    @foreach ($cuatrimestres as $cuatrimestre)
-                        <option value="{{ $cuatrimestre->pk_cuatrimestre }}">
-                            {{ $cuatrimestre->num_cuatri}}
-                        </option>
-                    @endforeach
-                </select>
-                <label class="text-gray-500 font-light">Materia:</label>
-                <select
-                class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    name="fk_materia" id="">
-                    <option value="">Seleccione una materia</option>
-                    @foreach ($materias as $materia)
-                        <option value="{{ $materia->pk_materia }}">
-                            {{ $materia->nombre}}
-                        </option>
-                    @endforeach
-                </select>
-                <label class="text-gray-500 font-light">Asignar docente:</label>
-                <select
-                class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    name="fk_docente" id="">
-                    <option value="">Seleccione un docente</option>
-                    @foreach ($docentes as $docente)
-                        <option value="{{ $docente->pk_usuario }}">
-                            {{ $docente->nombres}} {{ $docente->ap_paterno}} {{ $docente->ap_materno ?? ''}}
-                        </option>
-                    @endforeach
-                </select>
-                <label class="text-gray-500 font-light">Año:</label>
-                <input
-                    class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
-                    type="text" name="año" required maxlength="4" oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 4)"
-                >
-                <div class="flex justify-center mt-6">
-                    <button
-                        class="bg-teal-600 text-white w-full px-6 py-2 rounded-lg hover:bg-teal-700 shadow"
-                        type="submit"
-                    >
-                        Registrar
-                    </button>
                 </div>
+                {{-- <a href="{{route('coordinacion.lista-grupos')}}" class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-700 shadow">
+                Cancelar
+            </a> --}}
             </div>
-        </form>
-    </div>
-</section>
+            <form id="form-insertar" data-url="{{ route('coordinacion.guardar-grupo') }}" class="w-full gap-6"
+                action="" method="post">
+                @csrf
+                <div class="flex flex-col gap-2">
+                    <label class="text-gray-500 font-light">Nombre del grupo:</label>
+                    <select
+                        class="p-2 pr-10 border border-gray-300 rounded-lg w-full sm:w-full focus:outline-none focus:ring-2 focus:ring-teal-500 transition text-base appearance-none"
+                        name="nombre" id="">
+                        <option value="">Seleccione un nombre</option>
+                        <option value="A">A</option>
+                        <option value="B">B</option>
+                        <option value="C">C</option>
+                        <option value="D">D</option>
+                        <option value="E">E</option>
+                        <option value="F">F</option>
+
+                    </select>
+
+                    <label class="text-gray-500 font-light" for="">Carrera:</label>
+                    <select
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        name="fk_carrera" id="">
+                        <option value="">Seleccione una carrera</option>
+                        @foreach ($carreras as $carrera)
+                            <option value="{{ $carrera->pk_carrera }}">
+                                {{ $carrera->nombre }} - {{ $carrera->abreviatura }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <label class="text-gray-500 font-light">Cuatrimestre:</label>
+                    <select
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        name="fk_cuatrimestre" id="">
+                        <option value="">Seleccione un cuatrimestre</option>
+                        @foreach ($cuatrimestres as $cuatrimestre)
+                            <option value="{{ $cuatrimestre->pk_cuatrimestre }}">
+                                {{ $cuatrimestre->num_cuatri }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <label class="text-gray-500 font-light">Materia:</label>
+                    <select
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        name="fk_materia" id="">
+                        <option value="">Seleccione una materia</option>
+                        @foreach ($materias as $materia)
+                            <option value="{{ $materia->pk_materia }}">
+                                {{ $materia->nombre }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <label class="text-gray-500 font-light">Asignar docente:</label>
+                    <select
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        name="fk_docente" id="">
+                        <option value="">Seleccione un docente</option>
+                        @foreach ($docentes as $docente)
+                            <option value="{{ $docente->pk_usuario }}">
+                                {{ $docente->nombres }} {{ $docente->ap_paterno }} {{ $docente->ap_materno ?? '' }}
+                            </option>
+                        @endforeach
+                    </select>
+                    <label class="text-gray-500 font-light">Año:</label>
+                    <input
+                        class="p-2 border border-gray-300 rounded-lg w-full focus:outline-none focus:ring-2 focus:ring-teal-500 focus-border-transparent transition"
+                        type="text" name="año" required maxlength="4"
+                        oninput="this.value = this.value.replace(/[^0-9]/g, '').slice(0, 4)">
+                    <div class="flex justify-center mt-6">
+                        <button class="bg-teal-600 text-white w-full px-6 py-2 rounded-lg hover:bg-teal-700 shadow cursor-pointer"
+                            type="submit">
+                            Registrar
+                        </button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </section>
 
 @endsection
