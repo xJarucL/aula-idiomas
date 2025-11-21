0abd1da07542e5ee3f1183870d391c5f5d8891ec
Angel05m <aasm052002@gmail.com>
2025-11-21 10:17:35 -0700

    feat: detalles de diseño  de alumno

--- resources/views/alumno/inicio.blade.php
+++ resources/views/alumno/inicio.blade.php
@@ -7,7 +7,7 @@
     <section>
         <h1 class="text-3xl sm:text-5xl font-bold text-teal-700  ">Panel del Alumno</h1>
         <span class="text-sm sm:text-lg text-gray-500 sm:font-light mt-2">
-            Bienvenido/a, {{Auth::user()->nombres}} {{Auth::user()->ap_paterno}} {{Auth::user()->ap_materno ?? ''}}
+            Bienvenido/a, {{ Auth::user()->nombres }} {{ Auth::user()->ap_paterno }} {{ Auth::user()->ap_materno ?? '' }}
         </span>
     </section>
 
@@ -18,7 +18,7 @@
             <div class="flex justify-between items-center gap-2 h-full">
                 <div class="flex w-full flex-col gap-1 justify-center h-full">
                     <h3 class="text-sm sm:text-lg text-teal-700 font-medium">Calificacion</h3>
-                    <span class="text-teal-500 text-[20px] sm:text-2xl font-bold">{{$promedio ?? 'N/A'}}</span>
+                    <span class="text-teal-500 text-[20px] sm:text-2xl font-bold">{{ $promedio ?? 'N/A' }}</span>
                 </div>
             </div>
         </div>
@@ -27,7 +27,7 @@
             <div class="flex justify-between items-center h-full gap-2">
                 <div class="flex w-full flex-col gap-1 justify-center h-full">
                     <h3 class="text-sm sm:text-lg text-teal-700 font-medium">Materia</h3>
-                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{$materia}}</span>
+                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{ $materia }}</span>
                 </div>
             </div>
         </div>
@@ -40,7 +40,7 @@
                     </h3>
                     {{-- VISUALIZACION MOVIL --}}
                     <h3 class="text-sm sm:text-lg sm:hidden text-teal-700 font-medium">Act. pendientes</h3>
-                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{$count_pendientes}}</span>
+                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{ $count_pendientes }}</span>
 
                 </div>
             </div>
@@ -53,20 +53,20 @@
                     </h3>
                     <h3 class="text-sm sm:text-lg sm:hidden text-teal-700 font-medium">Act. terminadas</h3>
 
-                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{$count_entregadas}}</span>
+                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{ $count_entregadas }}</span>
                 </div>
             </div>
         </div>
 
         <!-- NO TERMINADAS -->
-         <div class="col-span-6 sm:col-span-2 bg-white p-3 border border-gray-200 rounded-lg shadow">
+        <div class="col-span-6 sm:col-span-2 bg-white p-3 border border-gray-200 rounded-lg shadow">
             <div class="flex justify-between items-center h-full w-full gap-2 ">
                 <div class="flex w-full flex-col gap-1 justify-center h-full ">
                     <h3 class="text-sm sm:text-lg hidden sm:inline-block text-teal-700 font-medium">No entregadas
                     </h3>
                     <h3 class="text-sm sm:text-lg sm:hidden text-teal-700 font-medium">Act. NO Terminadas</h3>
 
-                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{$count_no_entregadas}}</span>
+                    <span class="text-[20px] sm:text-2xl text-gray-700 font-bold">{{ $count_no_entregadas }}</span>
                 </div>
             </div>
         </div>
@@ -77,52 +77,53 @@
         <div class="flex flex-col-reverse sm:flex-row  sm:justify-between gap-3">
             <div class="bg-white p-5 w-full h-auto sm:w-2/3 border border-gray-200 rounded-lg shadow">
                 {{-- CONTENIDO PENDIENTE --}}
-                <h3 class="text-lg text-teal-700 mb-2 font-semibold">Actividades pendientes</h3>
-                <div class="max-h-79 sm:h-[800px] overflow-y-auto pr-1">
-                    @foreach($pendientes as $actividad)
-                        <x-card-actividad iconoA="question" color1="blue" nombreActividad="{{ $actividad['nom_actividad'] }}"
-                            tipo="{{ $actividad['tipo'] }}" fecha="{{ $actividad['fecha_fin'] }}" iconoEntregable="pending" color2="orange" link="{{ route('alumno.responder-actividad', $actividad['pk_actividad']) }}"
-                        />
+                <h3 class="text-lg text-teal-700 mb-2 font-semibold border-b border-gray-300">Actividades pendientes</h3>
+                <div class="h-[28rem] overflow-y-auto pr-1">
+                    @foreach ($pendientes as $actividad)
+                        <x-card-actividad iconoA="question" color1="blue"
+                            nombreActividad="{{ $actividad['nom_actividad'] }}" tipo="{{ $actividad['tipo'] }}"
+                            fecha="{{ $actividad['fecha_fin'] }}" iconoEntregable="pending" color2="orange"
+                            link="{{ route('alumno.responder-actividad', $actividad['pk_actividad']) }}" />
                     @endforeach
                 </div>
             </div>
-            <div class=" w-full sm:w-1/3 ">
+            <div class="flex flex-col w-full sm:w-1/3 ">
+                {{-- PROGRESO --}}
                 <div class="bg-white p-5 mb-5 sm:max-h-70 h-70 border border-gray-200 rounded-lg shadow">
                     <h3 class="text-lg text-teal-700 mb-2 font-semibold">Mi progreso</h3>
-                        <div>
-                            <h4 class="text-lg text-gray-400 mb-2">General</h4>
-                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
-                                <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                                    style="width: {{ $porcentaje_general }}%">
-                                    {{ $porcentaje_general }}%
-                                </div>
+                    <div>
+                        <h4 class="text-lg text-gray-400 mb-2">General</h4>
+                        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
+                            <div class="bg-blue-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                                style="width: {{ $porcentaje_general }}%">
+                                {{ $porcentaje_general }}%
                             </div>
                         </div>
-                        <div>
-                            <h4 class="text-lg text-gray-400 mb-2">Entregadas</h4>
-                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
-                                <div class="bg-green-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                                    style="width: {{ $porcentaje_entregadas }}%">
-                                    {{ $porcentaje_entregadas }}%
-                                </div>
+                    </div>
+                    <div>
+                        <h4 class="text-lg text-gray-400 mb-2">Entregadas</h4>
+                        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
+                            <div class="bg-green-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                                style="width: {{ $porcentaje_entregadas }}%">
+                                {{ $porcentaje_entregadas }}%
                             </div>
                         </div>
-                        <div>
-                            <h4 class="text-lg text-gray-400 mb-2">Pendientes</h4>
-                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
-                                <div class="bg-yellow-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                                    style="width: {{ $porcentaje_pendientes }}%">
-                                    {{ $porcentaje_pendientes }}%
-                                </div>
+                    </div>
+                    <div>
+                        <h4 class="text-lg text-gray-400 mb-2">Pendientes</h4>
+                        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
+                            <div class="bg-yellow-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                                style="width: {{ $porcentaje_pendientes }}%">
+                                {{ $porcentaje_pendientes }}%
                             </div>
                         </div>
-                        <div>
-                            <h4 class="text-lg text-gray-400 mb-2">No entregadas</h4>
-                            <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
-                                <div class="bg-red-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
-                                    style="width: {{ $porcentaje_no_entregadas }}%">
-                                    {{ $porcentaje_no_entregadas }}%
-                                </div>
+                    </div>
+                    <div>
+                        <h4 class="text-lg text-gray-400 mb-2">No entregadas</h4>
+                        <div class="w-full bg-gray-200 rounded-full dark:bg-gray-300">
+                            <div class="bg-red-600 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full"
+                                style="width: {{ $porcentaje_no_entregadas }}%">
+                                {{ $porcentaje_no_entregadas }}%
                             </div>
                         </div>
                     </div>
@@ -145,7 +146,24 @@
                         </div>
                     </div>
                 </a>
+                <a href="{{ route('alumno.lista-actividades') }}" class="mt-4">
+                    <div class=" bg-white p-4 h-auto border border-gray-300 rounded-lg shadow">
+                        <div class="flex justify-between">
+                            <div class="w-5/6 text-justify">
+                                <h3 class="text-lg text-teal-700 font-semibold">Mi lista de actividades</h3>
+                                <span class="text-sm text-gray-600 font-normal">Visualiza las actividades asignadas y su estado</span>
+                            </div>
+                            <div class="flex justify-center items-center w-1/6 pl-1">
+                                <svg width="48" height="48" class="text-center" viewBox="0 0 48 48" fill="none"
+                                    stroke="#2B877A" stroke-width="4" stroke-linecap="round" stroke-linejoin="round">
+                                    <polyline points="18,12 30,24 18,36" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
+        </div>
         </div>
     </section>
 @endsection
